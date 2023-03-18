4abb275e70026700a4e36ed86ccd9ec8731b0868
workavast <workavast@yandex.ru>
2023-03-18 13:17:06 +0300

    Fixed bug

--- Scripts/Drop/ItemDropper.cs
+++ Scripts/Drop/ItemDropper.cs
@@ -7,7 +7,6 @@ public class ItemDropper : MonoBehaviour
 {
     [SerializeField] private Vector3 dropPositionOffset;
     [SerializeField] private GameObject _collectablePrefab;
-    [SerializeField] private Transform _droppedItemsParent;
     [Space]
     [SerializeField] private WeightDrop _DropSet;
 
@@ -18,7 +17,7 @@ public class ItemDropper : MonoBehaviour
         foreach (var item in _DropSet.GetDropSet())
         {
             var position = transform.position + Vector3.right * UnityEngine.Random.Range(0, _dropRange);
-            var droppedObject = Instantiate(_collectablePrefab, position + dropPositionOffset, quaternion.identity, _droppedItemsParent);
+            var droppedObject = Instantiate(_collectablePrefab, position + dropPositionOffset, quaternion.identity);
             droppedObject.GetComponentInChildren<Collectable>().Item = item;
         }    
     }
